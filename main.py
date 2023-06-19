6613f356ac5765927fed83673bd06c167cb4afde
Dubrovin Vitaliy <vitaliy.dubrovin@gmail.com>
2023-06-19 20:17:15 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+def plus(a: int, b: int):
+    return a + b
+
+
+if __name__ == '__main__':
+    a = int(input('a - '))
+    b = int(input('b - '))
+    print(f"{a}+{b}={plus(a, b)}")
